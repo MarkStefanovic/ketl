1855f9aca1ab7f4fd55bca122620930ea7f7cfa2
Mark Stefanovic <markstefanovic@hotmail.com>
2021-12-20 04:09:36 -0800

    add ValidateJobs

---
+++ src/main/kotlin/ketl/domain/ValidateJobs.kt
@@ -0,0 +1,73 @@
+package ketl.domain
+
+import kotlin.time.ExperimentalTime
+
+@JvmInline
+value class JobValidationResult(val value: Map<String, Set<String>>) {
+  val hasErrors: Boolean
+    get() = value.values.any { it.isNotEmpty() }
+
+  val isOk: Boolean
+    get() = value.values.all { it.isEmpty() }
+
+  val errorMessage: String
+    get() = "Job Validation Errors:\n" +
+      value
+        .filter { it.value.isNotEmpty() }
+        .map { (jobName, errors) ->
+          "  $jobName:\n    ${errors.joinToString("\n    ")}"
+        }
+        .joinToString("\n")
+
+  override fun toString(): String =
+    "JobValidationResult [\n" +
+      value
+        .filter { it.value.isNotEmpty() }
+        .map { (jobName, errors) ->
+          if (errors.isEmpty()) {
+            "  $jobName: No errors"
+          } else {
+            "  $jobName:\n    ${errors.joinToString("\n    ")}"
+          }
+        }
+        .joinToString("\n") +
+      "\n]"
+}
+
+@ExperimentalTime
+fun validateJobs(jobs: Set<KETLJob>): JobValidationResult {
+  val allJobNames = jobs.map { it.name }
+
+  val validationErrors: Map<String, MutableList<String>> =
+    allJobNames.associateWith { mutableListOf() }
+
+  val duplicateJobNames: Map<String, Int> =
+    allJobNames
+      .groupingBy { it }
+      .eachCount()
+      .filter { it.value > 1 }
+
+  if (duplicateJobNames.isNotEmpty()) {
+    duplicateJobNames.forEach { (jobName, count) ->
+      validationErrors[jobName]?.add("$count jobs are named $jobName.")
+    }
+  }
+
+  jobs.forEach { job ->
+    val missingJobDeps = job.dependencies.filter { jobName ->
+      jobName !in allJobNames
+    }
+
+    if (missingJobDeps.isNotEmpty()) {
+      validationErrors[job.name]?.add(
+        "The following jobs are listed as dependencies, but they're not scheduled: ${missingJobDeps.joinToString(", ")}."
+      )
+    }
+  }
+
+  return JobValidationResult(
+    validationErrors
+      .map { it.key to it.value.toSet() }
+      .toMap()
+  )
+}
---
+++ src/test/kotlin/ketl/domain/ValidateJobsTest.kt
@@ -0,0 +1,100 @@
+package ketl.domain
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+
+@ExperimentalTime
+class DummyJob(
+  override val name: String,
+  override val schedule: Schedule,
+  override val timeout: Duration,
+  override val maxRetries: Int,
+  override val dependencies: Set<String>,
+) : KETLJob {
+  override suspend fun run(log: Log): Status {
+    TODO("Not yet implemented")
+  }
+}
+
+@ExperimentalTime
+class ValidateJobsTest {
+  @Test
+  fun given_duplicate_job_names_and_missing_dependencies() {
+    val jobs = setOf(
+      DummyJob(
+        name = "test_job_1",
+        schedule = every("every_day", frequency = Duration.days(1)),
+        timeout = Duration.minutes(15),
+        maxRetries = 1,
+        dependencies = setOf(),
+      ),
+      DummyJob(
+        name = "test_job_2",
+        schedule = every("every_day", frequency = Duration.days(1)),
+        timeout = Duration.minutes(15),
+        maxRetries = 1,
+        dependencies = setOf("test_job_1"),
+      ),
+      DummyJob(
+        name = "test_job_1",
+        schedule = every("every_day", frequency = Duration.days(2)),
+        timeout = Duration.minutes(15),
+        maxRetries = 1,
+        dependencies = setOf("test_job_4"),
+      ),
+      DummyJob(
+        name = "test_job_2",
+        schedule = every("every_day", frequency = Duration.days(3)),
+        timeout = Duration.minutes(15),
+        maxRetries = 1,
+        dependencies = setOf("test_job_1"),
+      ),
+      DummyJob(
+        name = "test_job_3",
+        schedule = every("every_day", frequency = Duration.days(1)),
+        timeout = Duration.minutes(15),
+        maxRetries = 1,
+        dependencies = setOf(),
+      ),
+    )
+
+    val validationResult = validateJobs(jobs)
+
+    assertEquals(
+      expected = true,
+      actual = validationResult.hasErrors,
+    )
+
+    assertEquals(
+      expected = false,
+      actual = validationResult.isOk,
+    )
+
+    assertEquals(
+      expected = """
+        |Job Validation Errors:
+        |  test_job_1:
+        |    2 jobs are named test_job_1.
+        |    The following jobs are listed as dependencies, but they're not scheduled: test_job_4.
+        |  test_job_2:
+        |    2 jobs are named test_job_2.
+      """.trimMargin(),
+      actual = validationResult.errorMessage,
+    )
+
+    assertEquals(
+      expected = """
+      |JobValidationResult [
+      |  test_job_1:
+      |    2 jobs are named test_job_1.
+      |    The following jobs are listed as dependencies, but they're not scheduled: test_job_4.
+      |  test_job_2:
+      |    2 jobs are named test_job_2.
+      |]
+    """.trimMargin(),
+      actual = validationResult.toString(),
+    )
+  }
+}
