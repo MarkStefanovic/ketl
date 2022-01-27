2348c44fe796041c2f3bed97497700000e610a81
Mark Stefanovic <markstefanovic@hotmail.com>
2022-01-27 02:00:48 -0800

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,14 @@ plugins {
 group = "me.mes"
 version = "1.0-SNAPSHOT"
 
-val kotlinVersion: String = "1.5.31"
+val kotlinVersion = "1.5.31"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-native-mt")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
 
@@ -22,31 +22,33 @@ dependencies {
 
     testImplementation("org.postgresql", "postgresql", "42.2.16")
 
-    testImplementation("com.zaxxer:HikariCP:5.0.0")
+    testImplementation("com.zaxxer:HikariCP:5.0.1")
 
-    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 }
 
 kotlinter {
     indentSize = 2
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+tasks {
+    test {
+        useJUnitPlatform()
+    }
 
-tasks.compileJava {
-    targetCompatibility = "16"
-}
+    compileJava {
+        targetCompatibility = "16"
+    }
 
-tasks.compileKotlin {
-    kotlinOptions.jvmTarget = "16"
-}
+    compileKotlin {
+        kotlinOptions.jvmTarget = "16"
+    }
 
-tasks.compileTestJava {
-    targetCompatibility = "16"
-}
+    compileTestJava {
+        targetCompatibility = "16"
+    }
 
-tasks.compileTestKotlin {
-    kotlinOptions.jvmTarget = "16"
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "16"
+    }
 }
