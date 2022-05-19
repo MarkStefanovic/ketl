8bab1e4f926523e0b30aab47f11669adb528fcc3
Mark Stefanovic <markstefanovic@hotmail.com>
2022-05-19 14:43:50 -0700

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.6.21"
     id("org.jmailen.kotlinter") version "3.10.0"
-    id( "org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
-    id("com.github.ben-manes.versions") version "0.41.0"
+    id( "org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
+    id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 group = "me.mes"
@@ -15,7 +17,9 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.10")
+    implementation("org.jetbrains.kotlin:kotlin-serialization:1.6.21")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.21")
 
     testImplementation("org.xerial:sqlite-jdbc:3.36.0.3")
 
@@ -23,7 +27,7 @@ dependencies {
 
     testImplementation("com.zaxxer:HikariCP:5.0.1")
 
-    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 }
 
 tasks {
@@ -46,4 +50,17 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "16"
     }
+
+    withType<DependencyUpdatesTask> {
+      rejectVersionIf {
+        isNonStable(candidate.version)
+      }
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+  val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+  val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+  val isStable = stableKeyword || regex.matches(version)
+  return isStable.not()
 }
